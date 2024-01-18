8dc8713b428ed2702498af108b3fbdf16eb56c5e
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2024-01-17 22:32:18 -0500

    fix: cmp working

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,65 +1,79 @@
 local M = {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-  },
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"hrsh7th/cmp-nvim-lua",
+	},
 }
 
 function M.config()
-  local cmp = require("cmp")
-  cmp.setup({
-    sources = {
-      { name = "nvim_lsp" },
-      { name = "buffer" },
-    },
-    mapping = cmp.mapping.preset.insert({
-      ["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
-      ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-      ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-      ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-      ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-      ["<C-e>"] = cmp.mapping({
-        i = cmp.mapping.abort(),
-        c = cmp.mapping.close(),
-      }),
+	local cmp = require("cmp")
+	local luasnip = require("luasnip")
 
-      -- Accept currently selected item. If none selected, `select` first item.
-      -- Set `select` to `false` to only confirm explicitly selected items.
-      ["<CR>"] = cmp.mapping.confirm({ select = false }),
-      --[[
-  ["<Tab>"] = cmp.mapping(function(fallback)
-  if cmp.visible() then
-  cmp.select_next_item()
-  elseif luasnip.expandable() then
-  luasnip.expand()
-  elseif luasnip.expand_or_jumpable() then
-  luasnip.expand_or_jump()
-  elseif check_backspace() then
-  fallback()
-  -- require("neotab").tabout()
-  else
-  fallback()
-  -- require("neotab").tabout()
-  end
-  end, {
-  "i",
-  "s",
-  }),
-  ["<S-Tab>"] = cmp.mapping(function(fallback)
-  if cmp.visible() then
-  cmp.select_prev_item()
-  elseif luasnip.jumpable(-1) then
-  luasnip.jump(-1)
-  else
-  fallback()
-  end
-  end, {
-  "i",
-  "s",
-  }),
-  --]]
-    }),
-  })
+	local check_backspace = function()
+		local col = vim.fn.col(".") - 1
+		return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+	end
+
+	cmp.setup({
+    snippet = {
+      expand = function(args)
+        luasnip.lsp_expand(args.body)
+      end
+    },
+		sources = {
+			{ name = "nvim_lsp" },
+			{ name = "luasnip" },
+			{ name = "buffer" },
+		},
+		mapping = cmp.mapping.preset.insert({
+			["<C-p>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+			["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+			["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+			["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+			["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+			["<C-e>"] = cmp.mapping({
+				i = cmp.mapping.abort(),
+				c = cmp.mapping.close(),
+			}),
+
+			-- Accept currently selected item. If none selected, `select` first item.
+			-- Set `select` to `false` to only confirm explicitly selected items.
+			["<CR>"] = cmp.mapping.confirm({ select = false }),
+			["<Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_next_item()
+				elseif luasnip.expandable() then
+					luasnip.expand()
+				elseif luasnip.expand_or_jumpable() then
+					luasnip.expand_or_jump()
+				elseif check_backspace() then
+					fallback()
+				-- require("neotab").tabout()
+				else
+					fallback()
+					-- require("neotab").tabout()
+				end
+			end, {
+				"i",
+				"s",
+			}),
+			["<S-Tab>"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_prev_item()
+				elseif luasnip.jumpable(-1) then
+					luasnip.jump(-1)
+				else
+					fallback()
+				end
+			end, {
+				"i",
+				"s",
+			}),
+		}),
+	})
 end
 
 return M
