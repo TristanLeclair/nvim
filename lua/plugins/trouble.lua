fac4e22ad3745a408f4d4524740c1ec8d06bc132
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2024-03-15 15:55:11 -0400

    add auto_close to trouble.nvim

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,23 +1,25 @@
 local M = {
-  "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 }
 
 function M.config()
-  local trouble = require("trouble")
+	local trouble = require("trouble").setup({
+		auto_close = true, -- automatically close the list when you have no diagnostics
+	})
 
-  Set_keymap("n", "<leader>lt", function()
-    trouble.toggle("workspace_diagnostics")
-  end, { noremap = true, silent = true }, "Trouble")
-  Set_keymap("n", "<leader>lj", function()
-    trouble.next({ skip_groups = true, jump = true })
-  end, { noremap = true, silent = true }, "Next diagnostic")
-  Set_keymap("n", "<leader>lk", function()
-    trouble.previous({ skip_groups = true, jump = true })
-  end, { noremap = true, silent = true }, "Previous diagnostic")
-  Set_keymap("n", "<leader>lx", function()
-    trouble.toggle("quickfix")
-  end, { noremap = true, silent = true }, "Close")
+	Set_keymap("n", "<leader>lt", function()
+		trouble.toggle("workspace_diagnostics")
+	end, { noremap = true, silent = true }, "Trouble")
+	Set_keymap("n", "<leader>lj", function()
+		trouble.next({ skip_groups = true, jump = true })
+	end, { noremap = true, silent = true }, "Next diagnostic")
+	Set_keymap("n", "<leader>lk", function()
+		trouble.previous({ skip_groups = true, jump = true })
+	end, { noremap = true, silent = true }, "Previous diagnostic")
+	Set_keymap("n", "<leader>lx", function()
+		trouble.toggle("quickfix")
+	end, { noremap = true, silent = true }, "Close")
 end
 
 return M
