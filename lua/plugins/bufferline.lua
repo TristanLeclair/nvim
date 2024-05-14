1c6f6f8f1e9e20942c24423c4cbc58bebb0eae31
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2024-05-14 14:30:16 -0400

    add scope for scopes tabs

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,14 +1,13 @@
 local M = {
   "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons", "catppuccin/nvim" },
+  dependencies = { "nvim-tree/nvim-web-devicons", "catppuccin/nvim", "tiagovla/scope.nvim" },
 }
 
 function M.config()
   require("bufferline").setup({
-    require("bufferline").setup({
-      highlights = require("catppuccin.groups.integrations.bufferline").get(),
-    }),
+    highlights = require("catppuccin.groups.integrations.bufferline").get(),
   })
+  require("scope").setup()
 end
 
 return M
