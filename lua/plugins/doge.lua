8173206229f00a6fbbc3d473d8b1a37e4964de42
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2024-05-14 14:37:59 -0400

    fix doge keymaps
    none set by default

--- lua/plugins/doge.lua
+++ lua/plugins/doge.lua
@@ -1,10 +1,21 @@
+--[[
+-- DOcumentation GEnerator
+--
+--
+]]
+--
+
+vim.g.doge_enable_mappings = 0
+
 local M = {
-	"kkoomen/vim-doge",
-	event = "BufRead",
+  "kkoomen/vim-doge",
+  event = "BufRead",
+  -- enabled = false,
+  build = ":call doge#install()",
 }
 
 function M.config()
-	vim.cmd([[call doge#install()]])
+  Set_keymap("n", "<leader>ld", "<Plug>(doge-generate)", "Generate documentation")
 end
 
 return M
