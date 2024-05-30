74a1a6b76ba22b4d93353dac35f1dc5368c0394b
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2024-05-30 15:21:26 -0400

    add hunk keybinds

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -9,6 +9,8 @@ function M.config()
   gs.setup()
 
   Set_keymap("n", "<leader>gb", gs.toggle_current_line_blame, "Toggle line blame")
+  Set_keymap("n", "<leader>gh[", gs.next_hunk, "Next hunk")
+  Set_keymap("n", "<leader>gh]", gs.prev_hunk, "Previous hunk")
 end
 
 return M
