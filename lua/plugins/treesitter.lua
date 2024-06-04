6dec2d03cecd11194bacc6f569a539fcee8a349d
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2024-06-04 01:37:08 -0400

    format treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,49 +1,49 @@
 local M = {
-	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
-	build = ":TSUpdate",
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
 }
 
 function M.config()
-	local configs = require("nvim-treesitter.configs")
+  local configs = require("nvim-treesitter.configs")
 
-	configs.setup({
-		ensure_installed = {
-			"bash",
-			"c",
-			"css",
-			"dockerfile",
-			"gitignore",
-			"html",
-			"javascript",
-			"json",
-			"lua",
-			"make",
-			"markdown",
-			"markdown_inline",
-			"python",
-			"sql",
-			"tsx",
-			"typescript",
-			"vimdoc",
-			"yaml",
-		},
-		auto_install = false,
-		ignore_install = {},
-		modules = {},
-		sync_install = false,
-		highlight = { enable = true },
-		indent = { enable = true },
-		incremental_selection = {
-			enable = true,
-			keymaps = {
-				init_selection = "<C-space>",
-				node_incremental = "<C-space>",
-				scope_incremental = false,
-				node_decremental = "<bs>",
-			},
-		},
-	})
+  configs.setup({
+    ensure_installed = {
+      "bash",
+      "c",
+      "css",
+      "dockerfile",
+      "gitignore",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "make",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "sql",
+      "tsx",
+      "typescript",
+      "vimdoc",
+      "yaml",
+    },
+    auto_install = false,
+    ignore_install = {},
+    modules = {},
+    sync_install = false,
+    highlight = { enable = true },
+    indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = "<C-space>",
+        node_incremental = "<C-space>",
+        scope_incremental = false,
+        node_decremental = "<bs>",
+      },
+    },
+  })
 end
 
 return M
