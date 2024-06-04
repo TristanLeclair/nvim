008c21210c8278318fcde719dcd61977c4e170d9
TristanLeclair <60434271+TristanLeclair@users.noreply.github.com>
2024-06-04 17:23:23 -0400

    remove execute on save dadbod

--- lua/plugins/dadbod.lua
+++ lua/plugins/dadbod.lua
@@ -17,6 +17,7 @@ function M.init()
   vim.g.db_ui_use_nerd_fonts = 1
   vim.g.db_ui_disable_progress_bar = 1
   vim.g.db_ui_use_nvim_notify = 1
+  vim.g.db_ui_execute_on_save = 0
 end
 
 function M.config()
